8065d88ecf61337b1e7034a3b74da189cb9e57e4
goyalkrishna104 <goyalkrishna104@gmail.com>
2019-11-19 01:12:11 +0530

    Added babel-plugin-import to optimize antd

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import 'antd/dist/antd.css';
+//import 'antd/dist/antd.css';
 import './css/index.js';
 import * as serviceWorker from './serviceWorker';
 
